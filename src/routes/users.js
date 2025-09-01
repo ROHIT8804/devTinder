d910fb9187b8c23d853722e57e2e228883846efb
NADL-ROHITK <>
2025-09-01 21:13:55 +0530

    bug fix for request recieved

--- src/routes/users.js
+++ src/routes/users.js
@@ -70,7 +70,7 @@ usersRouter.get("/users/requests/recieved", authToken, async (req, res) => {
 
         const recievedRequests = await ConnectionRequest.find({
             toUserId: loggedInUserId,
-        }).populate('fromUserId', 'firstName lastName emailId age');
+        }).populate('fromUserId', 'firstName lastName emailId age photoUrl skills about');
 
         if (recievedRequests.length === 0) {
             return res.status(404).send("No connection requests received");
